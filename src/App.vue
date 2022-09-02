ae00b14431252fa27c7353ddf32c5a8593983b2a
onesaber <1078907280@qq.com>
2022-09-02 17:13:32 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,34 @@
 <template>
   <div id="app">
-
-    <router-view></router-view >
+    <!-- 登录页面，layout -->
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
+// import { loginAPI } from './api/index.js'
+
 export default {
-  name: 'App'
+  name: 'App',
+
+  methods: {
+    // async await try catch
+    // async login () {
+    //   try {
+    //     const res = await loginAPI()
+    //     console.log(res)
+    //   } catch (error) {
+    //     console.log(error)
+    //   }
+    // }
+  }
 }
 </script>
 
 <style lang="less" scoped>
-
+.demo {
+  width: 400px;
+  padding: 20px;
+  border: 1px solid red;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,27 +1,34 @@
 import Vue from 'vue'
 import App from './App.vue'
-// index 文件可以被快捷路径所忽略
-// index命名的文件对于功能的可读性是非常差
-
-// 动态设置rem加载
-// 动态设置rem加载 - 在js代码中应该把所以的import引入模块的代码放到文件的最上方
+// 动态设置rem加载 - 在js代码中应该把所有的import引入模块的代码放到文件的最上方
 import 'amfe-flexible'
 
+// index 文件可以被快捷路径所忽略
+// index命名的文件对于功能可读性来说是非常差的
 import router from './router/index.js'
 import store from './store/index.js'
-// 引入字体图标
-import './style/icon.less'
 
-// 引入vant
-// import Vue from 'vue'
+// 引入icon字体图标
+import './styles/icon.less'
+
+// 全部引入vant
 import Vant from 'vant'
 import 'vant/lib/index.css'
 
 // 全局样式
-// 全局样式一定要卸载组件样式表的下面，全局样式可以影响组件样式
-import './style/index.less'
+// 全局样式一定要写在组件样式表的下面，其目的是为了保证全局样式可以影响组件样式
+import './styles/index.less'
+
+// 引入时间处理模块
+import './utils/dayjs'
+
+// 引入lodash
+import _ from 'lodash'
+// 构建lodash的全局方法
+Vue.prototype.$_ = _
 
 Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
